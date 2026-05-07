9c68f95d2712d67381e60584f8127f5caf2d5549
thedjchi <achiommino@gmail.com>
2026-05-07 01:30:50 -0400

    simplify example

--- example/example.py
+++ example/example.py
@@ -1,18 +1,17 @@
 from mcmaster_scraper.sync_api import get_products_from_url
-import pandas as pd
 
 
 def main():
     url = "https://www.mcmaster.com/products/springs/extension-springs-1~/system-of-measurement~inch"
     data = get_products_from_url(url)
 
-    stretch_length = 0.4
-    min_force = 0.5
-    max_force = 1
+    stretch_length = 6
+    min_force = 10
+    max_force = 10.5
 
-    length = pd.to_numeric(data[data.columns[2]])
-    extended_length = pd.to_numeric(data[data.columns[5]])
-    spring_rate = pd.to_numeric(data[data.columns[8]])
+    length = data[data.columns[2]]
+    extended_length = data[data.columns[5]]
+    spring_rate = data[data.columns[8]]
 
     displacement = stretch_length - length
     force = spring_rate * displacement
@@ -22,7 +21,7 @@ def main():
         (stretch_length < extended_length) &
         (force.between(min_force, max_force))
     ]
-    return filtered
+    return filtered[["Part Number", "Price"]]
 
 
 if __name__ == "__main__":
