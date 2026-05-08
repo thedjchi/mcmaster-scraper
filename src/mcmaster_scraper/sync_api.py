5b3e157309a7ffaf9e65c17f3f4f84cfc6dde40e
Alex <achiommino@gmail.com>
2026-05-08 13:38:27 -0400

    add black formatter

--- src/mcmaster_scraper/sync_api.py
+++ src/mcmaster_scraper/sync_api.py
@@ -5,38 +5,38 @@ from ._utils.event_loop_wrapper import run_in_loop_sync
 
 
 def get_products_from_url(url: str, refresh: bool = False) -> DataFrame:
-    """ Gets product tables from a McMaster-Carr URL.
+    """Gets product tables from a McMaster-Carr URL.
 
-        If there are multiple product tables, they will be merged,
-        and an additional "Product Type" column will be added.
+    If there are multiple product tables, they will be merged,
+    and an additional "Product Type" column will be added.
 
-        Parameters
-        ----------
-        url : str
-            The URL to scrape.
-            Must be a valid McMaster-Carr URL.
-            The product tables must be visible on the webpage.
-        refresh : bool, optional
-            Whether to refresh the cached data. Default is False.
+    Parameters
+    ----------
+    url : str
+        The URL to scrape.
+        Must be a valid McMaster-Carr URL.
+        The product tables must be visible on the webpage.
+    refresh : bool, optional
+        Whether to refresh the cached data. Default is False.
 
-        Returns
-        -------
-        DataFrame
-            A pandas DataFrame containing the combined product tables.
+    Returns
+    -------
+    DataFrame
+        A pandas DataFrame containing the combined product tables.
 
-        Raises
-        ------
-        ValueError
-            If the URL is not a valid McMaster-Carr URL.
-        """
+    Raises
+    ------
+    ValueError
+        If the URL is not a valid McMaster-Carr URL.
+    """
     return run_in_loop_sync(async_api.get_products_from_url(url, refresh))
 
 
 def get_products_from_urls(urls: list[str], refresh: bool = False) -> list[DataFrame]:
-    """ Gets product tables from a list of McMaster-Carr URLs.
+    """Gets product tables from a list of McMaster-Carr URLs.
 
-        See Also
-        --------
-        get_products_from_url
-        """
+    See Also
+    --------
+    get_products_from_url
+    """
     return run_in_loop_sync(async_api.get_products_from_urls(urls, refresh))
